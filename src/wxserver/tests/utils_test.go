e150615dce12847579d07132b10eb7becc8863d8
alan-w-255 <1649022341@qq.com>
2018-05-13 16:16:04 +0800

    添加创建菜单功能, 并通过测试

--- src/wxserver/tests/utils_test.go
+++ src/wxserver/tests/utils_test.go
@@ -1,8 +1,64 @@
 package test
 
 import (
+	util "alanwong/wxgo/src/wxserver/utils"
+	"fmt"
 	"testing"
+	"time"
+
+	"github.com/astaxie/beego"
 )
 
 func TestGetWXAccessTokenFromWX(t *testing.T) {
+	s, _ := util.GetWXAccessTokenFromWX()
+	if len(s) == 0 {
+		t.FailNow()
+	}
+}
+
+func TestGetWXAccessTokenFromLocal(t *testing.T) {
+	updateDuration, err := beego.AppConfig.Int64("wxaccesstokenupdateduration")
+	if err != nil {
+		fmt.Println(err.Error())
+		t.FailNow()
+	}
+	timer := time.NewTicker((time.Duration)(updateDuration))
+	util.UpdateWXAccessToken(timer)
+	time.Sleep(time.Second * 3)
+	s := util.GetWXAccessTokenFromLocal()
+	if len(s) == 0 {
+		t.Error("get an empty access token")
+		t.FailNow()
+	}
+	timer.Stop()
+}
+
+func TestUpdateWXAccessToken(t *testing.T) {
+	updateDuration, err := beego.AppConfig.Int64("wxaccesstokenupdateduration")
+	if err != nil {
+		fmt.Println(err.Error())
+		t.FailNow()
+	}
+	timer := time.NewTicker((time.Duration)(updateDuration))
+	util.UpdateWXAccessToken(timer)
+	time.Sleep(time.Second * 3)
+	s := util.GetWXAccessTokenFromLocal()
+	if len(s) == 0 {
+		t.Error("get an empty access token string")
+		t.FailNow()
+	}
+	timer.Stop()
+}
+
+func TestCreateMenu(t *testing.T) {
+	accessToken, err := util.GetWXAccessTokenFromWX()
+	if err != nil {
+		t.Error(err.Error())
+		t.FailNow()
+	}
+	beego.AppConfig.Set("wxaccesstoken", accessToken)
+	if err := util.CreateMenu(); err != nil {
+		t.Error(err.Error())
+		t.FailNow()
+	}
 }
--- src/wxserver/utils/utils.go
+++ src/wxserver/utils/utils.go
@@ -4,9 +4,11 @@ import (
 	"crypto/sha1"
 	"encoding/hex"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"sort"
 	"strings"
 	"time"
@@ -48,10 +50,11 @@ func IsWXServer(signature string, timestamp string, nonce string, echostr string
 }
 
 // GetWXAccessTokenFromWX 从微信后台获取微信access token
-func GetWXAccessTokenFromWX(grantType string) (string, error) {
+func GetWXAccessTokenFromWX() (string, error) {
 	tpl := beego.AppConfig.String("wxapiaccesstoken")
 	appid := beego.AppConfig.String("appid")
 	appsecret := beego.AppConfig.String("appsecret")
+	grantType := "client_credential"
 	urlstr := fmt.Sprintf(tpl, grantType, appid, appsecret)
 
 	l := logs.GetBeeLogger()
@@ -71,15 +74,15 @@ func GetWXAccessTokenFromWX(grantType string) (string, error) {
 		l.Error(err.Error())
 	}
 	if res.StatusCode == 200 {
-		l.Info("response status code: 200")
+		l.Info("get %s response status code: 200", urlstr)
 		var accessTokRes AccessTokenResponse
 		var errRes ErrorResponse
 		if err := json.Unmarshal(body, &accessTokRes); err == nil {
 			return accessTokRes.AccessToken, nil
 		} else if err := json.Unmarshal(body, &errRes); err == nil {
-			return "", fmt.Errorf("errcode: %d, errmsg: %s", errRes.Errcode, errRes.Errmsg)
+			return "", fmt.Errorf("get %s response errcode: %d, errmsg: %s", urlstr, errRes.Errcode, errRes.Errmsg)
 		} else {
-			return "", fmt.Errorf("error: %s", err.Error())
+			return "", fmt.Errorf("unable to unmashal %s response: %s error: %s", urlstr, string(body), err.Error())
 		}
 	}
 	return "", fmt.Errorf("error response status code: %d", res.StatusCode)
@@ -87,20 +90,20 @@ func GetWXAccessTokenFromWX(grantType string) (string, error) {
 }
 
 //GetWXAccessTokenFromLocal 从本地缓存获取access token
-func GetWXAccessTokenFromLocal(grantType string) string {
+func GetWXAccessTokenFromLocal() string {
 	return beego.AppConfig.String("wxaccesstoken")
 }
 
 // UpdateWXAccessToken 定时刷新微信access token
-func UpdateWXAccessToken(grantType string) {
-	updateduration, _ := beego.AppConfig.Int64("wxaccesstokenupdateduration")
-	timer := time.NewTicker((time.Duration)(updateduration) * time.Second)
+func UpdateWXAccessToken(timer *time.Ticker) {
 	l := logs.GetBeeLogger()
 	go func() {
+		// fmt.Println("start tick to update access token")
 		for range timer.C {
 			for i := 0; i < 3; i++ {
 				time.Sleep(time.Second)
-				s, err := GetWXAccessTokenFromWX("client_credential")
+				s, err := GetWXAccessTokenFromWX()
+				// fmt.Printf("update access_tokenn: %s\n", s)
 				if err != nil {
 					l.Error(err.Error())
 					if i == 3 {
@@ -117,4 +120,53 @@ func UpdateWXAccessToken(grantType string) {
 			}
 		}
 	}()
+}
+
+// CreateMenu 创建自定义菜单
+func CreateMenu() error {
+	urlTpl := "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=%s"
+	accessToke := GetWXAccessTokenFromLocal()
+
+	url := fmt.Sprintf(urlTpl, accessToke)
+	menuconfigpath := beego.AppConfig.String("wxmenuconfigpath")
+	config, err := os.Open(menuconfigpath)
+	logger := logs.GetBeeLogger()
+	if err != nil {
+		logger.Error("error occured while open %s", menuconfigpath)
+		logger.Error(err.Error())
+		errmsg := fmt.Sprintf("打开文件 %s 出错", menuconfigpath)
+		return errors.New(errmsg)
+	}
+	w, _ := ioutil.ReadAll(config)
+	res, err := http.Post(url, "application/json", strings.NewReader(string(w)))
+	if err != nil {
+		logger.Error("post 请求 %s 时出错!", url)
+		logger.Error(err.Error())
+		return errors.New("post 请求出错")
+	}
+	wxres := struct {
+		ErrorCode int    `json:"errcode"`
+		Errmsg    string `json:"errmsg"`
+	}{}
+	data, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		logger.Error("读取post %s 请求返回的数据出错", url)
+		return errors.New("读取请求返回数据出错")
+	}
+	if err := json.Unmarshal(data, &wxres); err != nil {
+		errmsg := fmt.Sprintf("解析post %s 返回数据出错", url)
+		logger.Info("statu code : %d", res.StatusCode)
+		logger.Error(err.Error())
+		logger.Error(errmsg)
+		logger.Error("返回数据为: %s", string(data))
+		return errors.New(errmsg)
+	}
+	if wxres.ErrorCode == 0 {
+		logger.Info("创建菜单成功")
+		return nil
+	} else {
+		logger.Error("创建菜单出错, errcode: %d, errmsg: %s", wxres.ErrorCode, wxres.Errmsg)
+		return errors.New("创建菜单出错")
+	}
 }
